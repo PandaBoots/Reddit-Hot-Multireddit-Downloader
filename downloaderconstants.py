9f5a6a82090d3c32c8b04bc9da8a737aed1ef492
PandaBoots <30639045+PandaBoots@users.noreply.github.com>
2018-05-27 15:36:00 -0700

    Update downloaderconstants.py

--- downloaderconstants.py
+++ downloaderconstants.py
@@ -1,20 +1,19 @@
-# the path to the directory where things will be downloaded to (keep in format of r'path/to/directory')
-#example: basepath = r':\Python\reddit\'
-basepath = r'D:\test\reddit'
+#example: basepath = r'D:\redditdownloads'
+#make sure there is no trailing '\' in the path as it will escape the final ' that ends the string
+basepath = r''
 
-# subreddits to be downloaded separated by '+'. You can get a string formatted this way by going to
-# old.reddit.com/subscriptions and clicking "multireddit from subscriptions" and pasting it here
-# example: substodownload = 'gonewild+nsfw+goddesses'
-substodownload ="60fpsporn+adorableporn+analgonewild+AnalHook+animearmpits+asaakira+AsianFeet+AsianHotties+AsianNSFW+AsianPorn+AsiansGoneWild+ass+Ass_to_Ass+asshole+AssholeBehindThong+assinthong+asstastic+BDSMLife+bodyperfection+Bondage+BorednIgnored+CelebrityFeet+Cuffed+cumfetish+cumsluts+datgap+dillion_harper+DirtyGaming+dirtysmall+dota2smut+EmilyBloom+facesitting+feet+FireCrotch+fitgirls+forcedorgasms+freeuse+FreeuseHentai+funsized+girlsinyogapants+goddesses+havoc_bot+hentai+hentaibondage+HugeDickTinyChick+insertions+JerkingInstruction+juicyasians+kpics+kpopfap+LegalTeens+lesBDSM+lilushandjobs+MaledomEmpire+MariahLeonne+milf+nextdoorasians+nsfw+NSFW_GIF+nsfw_gifs+NSFW_HTML5+NSFW_Japan+NSFW_Korea+NSFW_Snapchat+nsfw_videos+nsfwCelebArchive+nsfwcelebs+nsfwoutfits+O_Faces+Ohlympics+OnOff+paag+panties+Pantyfetish+Pee+PetiteGoneWild+PokePorn+porninfifteenseconds+pussy+PussyWedgie+randomsexiness+realasians+RealGirls+redheads+rule34+rule34_ass+Rule34Overwatch+SexyButNotPorn+shibari+ShittyLearnDota2+snapchat_sluts+snapleaks+StandingAsshole+StraightGirlsPlaying+Tentai+theperfectwife+throatpies+tightdresses+TinyTits+u_Dollywinks+u_fun-sized-asian+Unashamed+Upskirt+voluptuous+Vore+workgonewild+WtSSTaDaMiT+xsmallgirls+YogaPants+yuri+ZettaiRyouiki+ZettaiRyouikiIRL"
+# example: substodownload = 'wallpapers+pics+funny'
+substodownload = ""
 
-#The number of posts from the front page that you want for each subreddit
 #example: numposts = 7
 numposts = 7
 
 # reddit api credentials that are generated on reddit.com/prefs/apps
-#examples: client_id = '--------------' client_secret = '---------------------------'
-client_id = 'dSNykkqDpWiFjg'
-client_secret = 'woFyGjjr5UHOSUPToCM3U1ATG80'
+#Fake examples: 
+#client_id = 'dSNykkqOpIcFjg'
+#client_secret = 'woFyFjir5U07OSUPoCMOp1ATG80'
+client_id = ''
+client_secret = ''
 
 #verbose True/ False
 verbose = False
@@ -22,4 +21,5 @@ verbose = False
 # time between each update of downloader (in seconds) recommended value is 1800 (30mins)
 timebetweenupdates = 1800
 
+#recommended lower bound of score is 50 (depends on the size of subreddits you are downloading from)
 minscore = 50
